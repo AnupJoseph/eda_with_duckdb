bb4e6123cb9c9f7df721efcc68ab0fcc54834f0f
AnupJoseph <anup20joseph@gmail.com>
2023-03-05 13:08:09 +0000

    Applied all changes to plot the graph

--- utils.py
+++ utils.py
@@ -3,3 +3,6 @@ def wrap_to_string(df):
     num_cols_list = [f'"{i}"' for i in num_cols_list]
     numeric_columns = " ,".join(num_cols_list)
     return numeric_columns
+
+
+trimmer = lambda x:f"{x[:15]}..."
